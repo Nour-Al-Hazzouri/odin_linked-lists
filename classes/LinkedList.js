6d73eea98b72293674dd10ec3cc800414d80cc0f
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-07-24 10:10:38 +0300

    Add append(value) method

--- classes/LinkedList.js
+++ classes/LinkedList.js
@@ -8,5 +8,18 @@ class LinkedList {
   get Head() {
     return this.#headNode;
   }
+  append(value) {
+    const node = new Node();
+    node.value = value;
+    if (this.Head === null) {
+      this.head = node;
+    } else {
+      const tempNode = this.Head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = node;
+    }
+  }
 }
 export default LinkedList;
--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import LinkedList from "./classes/LinkedList.js";
 import Node from "./classes/Node.js";
+
+const list = new LinkedList();
+const node = new Node();
+console.log(list.Head);
+node.value = 10;
+console.log(node.Value);
+list.append(node);
+console.log(list.Head);
